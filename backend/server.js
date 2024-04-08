dffa7927c1085a4278459dd3d17359c0aca01863
Astamani <136489441+Astamani@users.noreply.github.com>
2024-04-08 23:23:42 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 import { app, server } from './socket/socket.js';
 import authRoutes from './routes/auth.routes.js'
@@ -9,12 +10,19 @@ import userRoutes from './routes/user.routes.js'
 
 import connectToMongodb from './db/connectToMongodb.js';
 
+const __dirname = path.resolve()
+
 dotenv.config();
 
 
 app.use(express.json()); //For parsing the data which is coming from the req.body (in the auth.controller.js file).
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend', 'dist', 'index.html'))
+})
 
 app.get("/api", (req, res) => {
     res.json({ success: "sdfds" })
